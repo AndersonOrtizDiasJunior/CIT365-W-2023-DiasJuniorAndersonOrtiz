6a15e430ad57415926f522b3c531745e08851ef3
AndersonOrtizDiasJunior <anderson_ortiz@hotmail.com.br>
2023-02-16 19:23:51 -0300

    feat: add 4 seed data

---
+++ ScriptureJournal/MyScriptureJournal/MyScriptureJournal/Models/SeedData.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MyScriptureJournalContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MyScriptureJournalContext>>()))
+        {
+            if (context == null || context.Entry == null)
+            {
+                throw new ArgumentNullException("Null MyScriptureJournalContext");
+            }
+
+            // Look for any movies.
+            if (context.Entry.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.Entry.AddRange(
+                new MyScriptureJournal.Models.Entry
+                {
+                    Book = "2 Nephi",
+                    Chapter = 25,
+                    InitialVerse = 26,
+                    FinalVerse = 26,
+                    Date = DateTime.Parse("2021-2-12"),
+                    Annotation = "Problaby the verse that repeat the word 'Christ' more times on all the scriptures."
+                },
+                new MyScriptureJournal.Models.Entry
+                {
+                    Book = "1 Nephi",
+                    Chapter = 3,
+                    InitialVerse = 7,
+                    FinalVerse = 7,
+                    Date = DateTime.Parse("2009-3-16"),
+                    Annotation = "The most know verse of the Book of Mormon."
+                },
+               new MyScriptureJournal.Models.Entry
+                                {
+                                    Book = "Enos",
+                                    Chapter = 1,
+                                    InitialVerse = 1,
+                                    FinalVerse = 27,
+                                    Date = DateTime.Parse("2022-10-23"),
+                                    Annotation = "This book teaches about the power of praying."
+                                },
+                new MyScriptureJournal.Models.Entry
+                {
+                    Book = "3 Nephi",
+                    Chapter = 11,
+                    InitialVerse = 8,
+                    FinalVerse = 10,
+                    Date = DateTime.Parse("2000-3-19"),
+                    Annotation = "Christ appear to the nephites the first time."
+                }
+
+            ); ;
+            context.SaveChanges();
+        }
+    }
+}
--- ScriptureJournal/MyScriptureJournal/MyScriptureJournal/Program.cs
+++ ScriptureJournal/MyScriptureJournal/MyScriptureJournal/Program.cs
@@ -9,6 +9,13 @@ builder.Services.AddDbContext<MyScriptureJournalContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
