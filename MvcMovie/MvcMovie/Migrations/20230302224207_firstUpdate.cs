c1f32538df1ae8b106f198e09ffc461cdb668115
AndersonOrtizDiasJunior <anderson_ortiz@hotmail.com.br>
2023-03-02 20:55:35 -0300

    add w9 assigment

---
+++ MvcMovie/MvcMovie/Migrations/20230302224207_firstUpdate.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "decimal(18, 2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "TEXT");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Movie",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Rating",
+                table: "Movie",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Movie",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 2)");
+        }
+    }
+}
---
+++ MvcMovie/MvcMovie/Models/SeedData.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MvcMovie.Data;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcMovieContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcMovieContext>>()))
+        {
+            // Look for any movies.
+            if (context.Movie.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.Movie.AddRange(
+                new Movie
+                {
+                    Title = "17 Miracles",
+                    ReleaseDate = DateTime.Parse("2011-6-3"),
+                    Genre = "Adventure",
+                    Rating = "PG",
+                    Price = 7.99M,
+                    ImageName = "17miracles.jpeg"
+                },
+                new Movie
+                {
+                    Title = "Ephraim's Rescue",
+                    ReleaseDate = DateTime.Parse("2013-5-31"),
+                    Genre = "Drama",
+                    Rating = "PG",
+                    Price = 8.99M,
+                    ImageName = "ephraim.jpeg"
+
+                },
+                new Movie
+                {
+                    Title = "Singles Ward",
+                    ReleaseDate = DateTime.Parse("2002-1-30"),
+                    Genre = "Comedy",
+                    Rating = "PG",
+                    Price = 9.99M,
+                    ImageName = "singlesWard.jpeg"
+                 
+                },
+                new Movie
+                {
+                    Title = "The Saratov Approach",
+                    ReleaseDate = DateTime.Parse("2013-10-1"),
+                    Genre = "Action",
+                    Rating = "PG-13",
+                    Price = 3.99M,
+                    ImageName = "saratov.jpeg"
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
